76fffd5402a75496dc82fc45d5165c0a2323770e
TheTeejers <tjloughry@gmail.com>
2020-06-09 13:41:48 -0500

    K1 data being pulled

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+// const {variableName} = require('/Users/terranceloughry/Documents/TheTeejers/198-web-scraper-link-preview/react-app/src/App.js');
 const functions = require('firebase-functions');
 const cors = require('cors')({ origin: true});
 
@@ -9,7 +10,7 @@ const fetch = require('node-fetch');
 const scrapeMetatags = (text) => {
 
 
-
+    console.log("this one " + text);
     const urls = Array.from( getUrls(text) );
 
     const requests = urls.map(async url => {
@@ -18,13 +19,13 @@ const scrapeMetatags = (text) => {
 
         const html = await res.text();
         const $ = cheerio.load(html);
-        
-        const getMetatag = (name) =>  
-            $(`meta[name=${name}]`).attr('content') ||  
-            $(`meta[property="og:${name}"]`).attr('content') ||  
+
+        const getMetatag = (name) =>
+            $(`meta[name=${name}]`).attr('content') ||
+            $(`meta[property="og:${name}"]`).attr('content') ||
             $(`meta[property="twitter:${name}"]`).attr('content');
 
-        return { 
+        return {
             url,
             title: $('title').first().text(),
             favicon: $('link[rel="shortcut icon"]').attr('href'),
@@ -44,28 +45,46 @@ const scrapeMetatags = (text) => {
 
 
 
-
-
-
-
-
-
 const puppeteer = require('puppeteer');
 
 
-const scrapeImages = async (username) => {
-    const browser = await puppeteer.launch( { headless: true });
+const scrapeImages = async (creds) => {
+    const browser = await puppeteer.launch( { headless: true});
+    // const browser = await puppeteer.launch( { headless: true, mode: 'no-cors' });
     const page = await browser.newPage();
-    
-    await page.goto('https://www.instagram.com/accounts/login/');
+    // console.log("this one " + text);
+
+
+    await page.waitFor(5000);
+
+    // await page.goto('https://www.instagram.com/accounts/login/');
+    const response = await page.goto('https://k1austin.clubspeedtiming.com/sp_center/signin.aspx');
+
 
 
     // Login form
     await page.screenshot({path: '1.png'});
+    console.log("looking");
+    console.log(creds);
 
-    await page.type('[name=username]', 'fireship_dev');
 
-    await page.type('[name=password]', 'some-password');
+
+    // await page.type('[name=username]', 'theteejers');
+    // await page.type('[name=tbxUserName]', 'TJ Loughry');
+    await page.type('[name=tbxUserName]', creds.text);
+    // await page.type('[name=tbxUserName]', 'Will Courtright');
+
+
+
+    // console.log("name entered");
+
+    // await page.type('[name=password]', 'CatsRKool2!');
+    // await page.type('[name=tbxPassword]', 'CatsRKool2!');
+    await page.type('[name=tbxPassword]', creds.password);
+
+
+
+    // console.log("password entered");
 
     await page.screenshot({path: '2.png'});
 
@@ -73,11 +92,19 @@ const scrapeImages = async (username) => {
 
     // Social Page
 
-    await page.waitFor(5000);
+    // await page.waitFor(5000);
+    //
+    // await page.click('[type=button]');
 
-    await page.goto(`https://www.instagram.com/${username}`);
+    // await page.waitFor(5000);
 
-    await page.waitForSelector('img ', {
+    console.log(page.url());
+    console.log(typeof page.url());
+    // console.log({ text });
+
+    await page.goto(page.url());
+
+    await page.waitForSelector('tbody ', {
         visible: true,
     });
 
@@ -86,20 +113,103 @@ const scrapeImages = async (username) => {
 
 
     // Execute code in the DOM
-    const data = await page.evaluate( () => {
+    // const data = await page.evaluate( () => {
+    //
+    //     const images = document.querySelectorAll('a');
+    //
+    //     const urls = Array.from(images).map(a => a.href);
+    //     const urls1 = Array.from(images).map(a => a);
+    //
+    //     // return images
+    //     return urls1
+    //     // console.log(urls1);
+    // });
+    //
+    // await browser.close();
+    const raceCounts = await page.$$eval('tr.Normal', trs => trs.length);
+    // console.log(raceCounts)
+    let dataArray = []
 
-        const images = document.querySelectorAll('img');
+    const [name] = await page.$x('//*[@id="lblRacerName"]');
+    const txt = await name.getProperty('textContent');
+    const nameData = await txt.jsonValue();
+    console.log({nameData});
 
-        const urls = Array.from(images).map(v => v.src);
 
-        return urls
-    });
-  
+    var i;
+    for (i = 2; i < raceCounts+2; i++) {
+      var arrayName = []
+      // console.log(i);
+      const raceNumber = -1*(i - raceCounts - 2)
+
+
+      const [el] = await page.$x('//*[@id="dg"]/tbody/tr['+i+']/td[1]/a');
+      const txt = await el.getProperty('href');
+      const heatLink = await txt.jsonValue();
+
+
+      const [el0] = await page.$x('//*[@id="dg"]/tbody/tr['+i+']/td[1]');
+      const txt0 = await el0.getProperty('textContent');
+      const heatTypeAndKart = await txt0.jsonValue();
+      let heatData = heatTypeAndKart.split(' - Kart ')
+      const heatType = heatData[0]
+      const kartNumberLong = heatData[1]
+      if (kartNumberLong < 10) {
+        var kartNumber = "0".concat(kartNumberLong)
+      } else {
+        var kartNumber = kartNumberLong
+      }
+
+      const [el1] = await page.$x('//*[@id="dg"]/tbody/tr['+i+']/td[2]');
+      const txt1 = await el1.getProperty('textContent');
+      const dateTimeLong = await txt1.jsonValue();
+      const dateTimeShort = dateTimeLong.trim();
+      const dateTimeSplit = dateTimeShort.split(" ")
+      const date = dateTimeSplit[0]
+      const time = dateTimeSplit.splice(1,2).toString().replace(",", " ")
+
+
+      const [el2] = await page.$x('//*[@id="dg"]/tbody/tr['+i+']/td[3]');
+      const txt2 = await el2.getProperty('textContent');
+      const k1rs = await txt2.jsonValue();
+
+      const [el3] = await page.$x('//*[@id="dg"]/tbody/tr['+i+']/td[4]');
+      const txt3 = await el3.getProperty('textContent');
+      const bestTime = await txt3.jsonValue();
+
+      const [el4] = await page.$x('//*[@id="dg"]/tbody/tr['+i+']/td[5]');
+      const txt4 = await el4.getProperty('textContent');
+      const positionLong = await txt4.jsonValue();
+      const position = positionLong.trim();
+
+
+
+
+      // arrayName.push(heatType1, dateTime, k1rs, bestTime, position)
+      var raceData = {raceNumber, heatLink, heatType, kartNumber, date, time, k1rs, bestTime, position, nameData}
+      // console.log(arrayName);
+      // console.log(typeof raceData);
+      // console.log(raceData);
+      dataArray.push(raceData)
+
+    }
+
+  // console.log(dataArray[50]);
+
+
+    // fs.writeFile("./public/raceData.json", JSON.stringify(dataArray), "utf8", (err, data) => {
+    //   if (err) {
+    //   console.error(err);
+    // } else {
+    //   console.log("created!");
+    // }});
+
     await browser.close();
 
-    console.log(data)
 
-    return data;
+    // console.log("Here " + dataArray)
+
+    return dataArray;
 }
 
 
@@ -109,12 +219,14 @@ exports.scraper = functions.https.onRequest((request, response) => {
     cors(request, response, async () => {
 
 
-        const body = JSON.parse(request.body);
-        // const data = await scrapeMetatags(body.text);
+      const body = JSON.parse(request.body);
+        // const body = JSON.parse(request.body);
+        // const data1 = await scrapeMetatags(body.password);
 
-        const data = await scrapeImages(body.text);
+        const data = await scrapeImages(body.creds);
 
         response.send(data)
+
 
 
     });
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
 
+const variableName = 'variableValue';
+
 function App() {
 
   return (
@@ -10,29 +12,43 @@ function App() {
   );
 }
 
+// module.exports = variableName
+
+
+
 function LinkPreview() {
 
   const [text, setText] = useState('');
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false); 
   const [links, setLinks] = useState([]);
+  const [password, setPassword] = useState('');
+  const [racerName, setRacerName] = useState('');
+
+  var creds = {}
+  creds.text = text
+  creds.password = password
 
   const handleSubmit = async(evt) => {
     setLoading(true)
     evt.preventDefault();
     console.log(`Submitting ${text}`);
 
-    const res = await fetch('http://localhost:5000/fireship-lessons/us-central1/scraper', { 
-        method: 'POST', 
-        body: JSON.stringify({ text }) 
+    const res = await fetch('http://localhost:5000/k1data/us-central1/scraper', {
+        method: 'POST',
+        body: JSON.stringify({creds}),
+        // username: JSON.stringify({racerName})
     });
 
     const data = await res.json();
-
+    setRacerName(data[0].nameData)
     console.log(data)
+    console.log(racerName);
 
     setLinks(data);
     setLoading(false)
   }
+
+
 
   return (
     <div>
@@ -40,23 +56,42 @@ function LinkPreview() {
       {/* Try this: <pre>a post with some links https://fireship.io and https://fireship.io/courses/javascript/</pre> */}
 
       <form onSubmit={handleSubmit}>
-        <textarea rows="4" cols="50" 
-          type="text" 
+        <input rows="1" cols="50"
+          placeholder="Racer ID"
+          type="text"
           value={text}
           onChange={e => setText(e.target.value)}>
 
-        </textarea>
+        </input>
+        <input rows="1" cols="50"
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}>
+
+        </input>
         <br />
         <input type="submit" value="Submit" />
         </form>
 
-        <h2>Preview</h2>
-        <p>{text}</p>
+        <h2>Racer  {racerName}</h2>
 
-        {loading &&  <h3>Fetching link previews... 🤔🤔🤔</h3> }
+        {loading &&  <h3>Fetching racer data...</h3> }
 
-
-        { links.map(obj => <PreviewCard key={obj.url} linkData={obj} />) }
+        <table>
+          <tbody>
+            <tr>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Race Number</th>
+              <th>Race Type</th>
+              <th>Kart Number</th>
+              <th>Position</th>
+              <th>BestTime</th>
+            </tr>
+          </tbody>
+        </table>
+        { links.map(obj => <PreviewCard key={obj.raceNumber} linkData={obj} />) }
 
 
     </div>
@@ -65,14 +100,22 @@ function LinkPreview() {
 
 function PreviewCard({ linkData }) {
   return (
-    <a className="preview" href={linkData.url}>
-      <img src={linkData.image} />
-      <div>
-        <h4>{linkData.title}</h4>
-        <p>{linkData.description}</p>
-      </div>
-    </a>
+    <table>
+      <tbody>
+        <tr>
+          <td>{linkData.date}</td>
+          <td>{linkData.time}</td>
+          <td>{linkData.raceNumber}</td>
+          <td><a href={linkData.heatLink} target="_blank">{linkData.heatType}</a></td>
+          <td>{linkData.kartNumber}</td>
+          <td>{linkData.position}</td>
+          <td>{linkData.bestTime}</td>
+        </tr>
+      </tbody>
+    </table>
+
   )
 }
+
 
 export default App;
